efc62abbde9fe959ab6dd4f85fa35238c7adeb76
Preetam Bhosle <preetb123@gmail.com>
2018-02-03 23:40:22 +0530

    Refactored app to use default imports with typescripts 2.7 using esModuleInterop

--- app.ts
+++ app.ts
@@ -1,35 +1,35 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import { Request, Response } from 'express';
+import express from "express";
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
 
-class App{
-    constructor(){
-        this.app = express();
-        this.config();
-        this.routes();
-    }
-    public app: express.Application;
-    
-    private config(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+class App {
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+  public app: express.Application;
 
-    private routes(): void{
-        const router = express.Router();
-        router.get('/', (req: Request, res: Response) => {
-            res.status(200).send({
-                message: 'Hello world'
-            })
-        });   
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 
-        router.post('/', (req: Request, res: Response) => {
-            const data = req.body;
-            res.status(200).send(data);
-        });
+  private routes(): void {
+    const router = express.Router();
+    router.get("/", (req: Request, res: Response) => {
+      res.status(200).send({
+        message: "Hello world"
+      });
+    });
 
-        this.app.use('/', router);
-    }
+    router.post("/", (req: Request, res: Response) => {
+      const data = req.body;
+      res.status(200).send(data);
+    });
+
+    this.app.use("/", router);
+  }
 }
 
 export default new App().app;
--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
-import app from './app';
+import app from "./app";
 const port: number = 4040;
 
-app.listen(port, () => console.log('express server listening on port: ' + port));
+app.listen(port, () =>
+  console.log("express server listening on port: " + port)
+);
